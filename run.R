f7cadc539e1772a3ef690fbd63e767d6de166b68
remiparis <remi.paris@ucdconnect.ie>
2019-06-04 15:09:13 +0200

    Update run.R

--- run.R
+++ run.R
@@ -1,1 +1,9 @@
- library(shiny) port <- Sys.getenv('PORT') shiny::runApp(   appDir = getwd(),   host = '0.0.0.0',  port = as.numeric(port) )
+library(shiny)
+
+port <- Sys.getenv('PORT')
+
+shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
+)
